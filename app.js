965b981dbb7eea013be6ee1d19cea75104a74e13
PJSalter <psalter88@googlemail.com>
2022-03-30 23:15:26 +0100

    start snake length object

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-
+// DOM for picking up the board for the snake game.
+const BoardForTheSnake = document.querySelector('#game-board');
 // making the canvas
 //getting the canvas 2d content, which means that it will be drawn into a 2D space.
 const displayBoardEffect = gameCanvas.getContext("2d");
+
+//creating coordinates for as an object that will define the length of the snake.
+// x and y coordinates.
+// creating the snake in middle of canvas.
+
+let mySnakeGrowth = [
+    {x: 200, y: 200},
+    {x: 190, y: 200}
+]
 

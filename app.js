e067f5575aa2aa0f2389527f9edf5d6599043a67
PJSalter <psalter88@googlemail.com>
2022-04-16 23:32:21 +0100

    creating the speed on movement once function is activated for snake interaction

--- app.js
+++ app.js
@@ -91,7 +91,8 @@ window.addEventListener(
     document.addEventListener("keyup", moveThatSnake);
 
     // now I will create a function that will update the board and the HTML is going to draw
-    reformToUpdate();
+    setInterval(reformToUpdate, 1000 / 10); // this means it will move per 100 milliseconds at speed.
+    //reformToUpdate();
   },
   false
 );
